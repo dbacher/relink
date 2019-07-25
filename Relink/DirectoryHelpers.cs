af7f9690f040da53267b08f1ef4e85ee98a52ea1
Dave Bacher <daveb@battlebazaar.net>
2019-07-25 08:53:15 -0400

    Initial from working copy.

---
+++ Relink/DirectoryHelpers.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+
+namespace Relink
+{
+    static class DirectoryHelpers
+    {
+        private static void CopyFile(FileInfo file, string dest)
+        {
+            Console.Write(".");
+
+            var fileSecurity = file.GetAccessControl();
+
+            fileSecurity.SetAccessRuleProtection(true, true);
+
+            file.CopyTo(dest, true);
+
+            var copiedFile = new FileInfo(dest);
+
+            copiedFile.SetAccessControl(fileSecurity);
+        }
+
+        /// <summary>
+        /// Copy directory recursive with permissions, overwrite existing
+        /// </summary>
+        /// <param name="sourceFolder"></param>
+        /// <param name="destinationFolder"></param>
+        public static void CopyDirectory(string sourceFolder, string destinationFolder)
+        {
+            var sourceDirectory = new DirectoryInfo(sourceFolder);
+            if(!sourceDirectory.Exists)
+                throw new DirectoryNotFoundException($"Source folder not found: {sourceFolder}");
+
+            var destinationDirectory = !Directory.Exists(destinationFolder)
+                ? Directory.CreateDirectory(destinationFolder)
+                : new DirectoryInfo(destinationFolder);
+
+            CopyDirectory(sourceDirectory, destinationDirectory);
+        }
+
+        public static void CopyDirectory(DirectoryInfo sourceDirectory, DirectoryInfo destinationDirectory)
+        {
+            if(sourceDirectory == null)
+                throw new ArgumentException(nameof(sourceDirectory));
+            if(destinationDirectory == null)
+                throw new ArgumentException(nameof(destinationDirectory));
+
+            var security = sourceDirectory.GetAccessControl();
+
+            security.SetAccessRuleProtection(true, true);
+            destinationDirectory.SetAccessControl(security);
+
+            var dirsToCopy = sourceDirectory.GetDirectories();
+            foreach(var dirToCopy in dirsToCopy)
+            {
+                var destSubDirPath = Path.Combine(destinationDirectory.FullName, dirToCopy.Name);
+                var destinationSubDir = !Directory.Exists(destSubDirPath)
+                    ? Directory.CreateDirectory(destSubDirPath)
+                    : new DirectoryInfo(destSubDirPath);
+                CopyDirectory(dirToCopy, destinationSubDir);
+            }
+
+            var filesToCopy = sourceDirectory.GetFiles();
+
+            foreach(var file in filesToCopy)
+            {
+                CopyFile(file, Path.Combine(destinationDirectory.FullName, file.Name));
+            }
+        }
+
+        public static void CopyFile(string source, string dest)
+        {
+            var file = new FileInfo(source);
+            CopyFile(file, dest);
+        }
+    }
+}
---
+++ Relink/Program.cs
@@ -0,0 +1,126 @@
+﻿using SymbolicLinkSupport;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Relink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Find all links            
+            foreach(var arg in args)
+            {
+                foreach(var link in GetDirectoryLinks(arg))
+                {
+                    DoDirectoryCopy(link);
+                }
+
+                foreach(var link in GetFileLinks(arg))
+                {
+                    DoFileCopy(link);
+                }
+            }
+        }
+
+        private static void DoFileCopy(string link)
+        {
+            Console.WriteLine($"---{link}");
+
+            var file = new FileInfo(link);
+            var target = file.GetSymbolicLinkTarget();
+            var tempName = GetRandomName(link);
+
+            Console.WriteLine($" -> {link} to {target}");
+
+            DirectoryHelpers.CopyFile(link, tempName);
+
+            Console.WriteLine($" Deleting {link}");
+            File.Delete(link);
+
+            Console.WriteLine($" Renaming {tempName} to {link}");
+            File.Move(tempName, link);
+        }
+
+        public static void DoDirectoryCopy(string link)
+        {
+            var dirInfo = new DirectoryInfo(link);
+            var target = dirInfo.GetSymbolicLinkTarget();
+            var tempName = GetRandomName(link);
+
+            Console.WriteLine($"Found {link} ({target})");
+            Console.WriteLine($"  => Copying {target} to {tempName}");
+
+            DirectoryHelpers.CopyDirectory(target, tempName);
+            Console.WriteLine();
+
+            Console.WriteLine($"  => Deleting {link}");
+            Directory.Delete(link);
+
+            Console.WriteLine($"  => Moving {tempName} to {link}");
+            Directory.Move(tempName, link);
+        }
+
+
+        static readonly Random rng = new Random();
+
+        static string GetRandomName(string x)
+        {
+            var sb = new StringBuilder(x);
+
+            const string allowed = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            sb.Append("-");
+            for(int i = 0; i < 5; i++)
+            {
+                sb.Append(allowed[rng.Next(allowed.Length - 1)]);
+            }
+
+            return sb.ToString();
+        }
+
+        static IEnumerable<string> GetDirectoryLinks(string path)
+        {
+            var allDirs = Directory.EnumerateDirectories(path);
+            foreach(var currentDir in allDirs)
+            {
+                var isLink = false;
+                try
+                {
+                    var dirInfo = new DirectoryInfo(currentDir);
+
+                    isLink = (dirInfo.IsSymbolicLink() && dirInfo.IsSymbolicLinkValid());
+                } catch(UnauthorizedAccessException uae)
+                {
+                    Console.WriteLine($"*** Couldn't access {currentDir} - {uae.ToString()}");
+                }
+
+                if(isLink)
+                    yield return currentDir;
+            }
+        }
+
+        static IEnumerable<string> GetFileLinks(string path)
+        {
+            var allFiles = Directory.EnumerateFiles(path);
+            foreach(var currentFile in allFiles)
+            {
+                var isLink = false;
+                try
+                {
+                    var fileInfo = new FileInfo(currentFile);
+
+                    isLink = (fileInfo.IsSymbolicLink() && fileInfo.IsSymbolicLinkValid());
+                } catch(UnauthorizedAccessException uae)
+                {
+                    Console.WriteLine($"*** Couldn't access {currentFile} - {uae.ToString()}");
+                }
+
+                if(isLink)
+                    yield return currentFile;
+            }
+        }
+    }
+}
